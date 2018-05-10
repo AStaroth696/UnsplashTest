e33c42e3f5be868eb28328c0a0132ecfb2dfb448
Oleg <tykans@gmail.com>
2018-05-10 12:15:38 +0300

    Second commit

--- app/src/main/java/com/example/android/unsplashtest/activity/DetailActivity.java
+++ app/src/main/java/com/example/android/unsplashtest/activity/DetailActivity.java
@@ -3,14 +3,22 @@ package com.example.android.unsplashtest.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.android.unsplashtest.R;
 import com.example.android.unsplashtest.presenter.DetailPresenter;
 import com.example.android.unsplashtest.service.DaggerDetailActivityComponent;
 import com.example.android.unsplashtest.service.DetailActivityModule;
+import com.github.chrisbanes.photoview.PhotoView;
 
 import javax.inject.Inject;
+
 
 public class DetailActivity extends AppCompatActivity {
     public static final String PHOTO_ID = "photo id";
@@ -21,7 +29,8 @@ public class DetailActivity extends AppCompatActivity {
     private String photoId;
     private String photoUrl;
     private String photoDownloadUrl;
-    private ImageView largePhoto;
+    private WebView webView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +43,9 @@ public class DetailActivity extends AppCompatActivity {
             photoUrl = getIntent().getStringExtra(PHOTO_URL);
             photoDownloadUrl = getIntent().getStringExtra(PHOTO_DOWNLOAD_URL);
         }
-        largePhoto = findViewById(R.id.large_photo);
+        progressBar = findViewById(R.id.download_progress_bar);
+        webView = findViewById(R.id.download_web_view);
+        PhotoView largePhoto = findViewById(R.id.large_photo);
         presenter.loadPhotoIntoContainer(largePhoto, photoUrl);
     }
 
@@ -42,5 +53,27 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.detail_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_download:
+                presenter.downloadPhoto(photoDownloadUrl, photoId, webView);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showToast(String text){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    public void setProgressBarVisible() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    public void setProgressBarInvisible() {
+        progressBar.setVisibility(View.INVISIBLE);
     }
 }
--- app/src/main/java/com/example/android/unsplashtest/presenter/DetailPresenter.java
+++ app/src/main/java/com/example/android/unsplashtest/presenter/DetailPresenter.java
@@ -1,10 +1,23 @@
 package com.example.android.unsplashtest.presenter;
 
-import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Environment;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.android.unsplashtest.activity.DetailActivity;
 
+import com.github.chrisbanes.photoview.PhotoView;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class DetailPresenter{
     private DetailActivity context;
@@ -13,7 +26,63 @@ public class DetailPresenter{
         this.context = detailActivity;
     }
 
-    public void loadPhotoIntoContainer(ImageView photo, String url){
-        Picasso.with(context).load(url).into(photo);
+    public void loadPhotoIntoContainer(PhotoView photo, String url){
+        context.setProgressBarVisible();
+        Picasso.with(context).load(url).into(photo, new Callback() {
+            @Override
+            public void onSuccess() {
+                context.setProgressBarInvisible();
+            }
+
+            @Override
+            public void onError() {
+                context.setProgressBarInvisible();
+            }
+        });
+    }
+
+    public void downloadPhoto(String url, final String id, WebView webView){
+        context.setProgressBarVisible();
+        context.showToast("Downloading...");
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.d("DOWNLOADING", "url");
+                if (url.contains("images.unsplash.com")){
+                    Picasso.with(context).load(url).into(new Target() {
+                        @Override
+                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                            Log.d("DOWNLOADING", "downloading file");
+                            File file = new File(Environment.getExternalStorageDirectory().getPath()
+                                    + "/" + id);
+                            try {
+                                file.createNewFile();
+                                FileOutputStream fos = new FileOutputStream(file);
+                                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                                fos.close();
+                                context.setProgressBarInvisible();
+                                context.showToast("Photo downloaded");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onBitmapFailed(Drawable errorDrawable) {
+                            Log.d("DOWNLOADING", "downloading failed");
+                        }
+
+                        @Override
+                        public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                        }
+                    });
+
+                }
+                view.loadUrl(url);
+                return false;
+            }
+        });
     }
 }
